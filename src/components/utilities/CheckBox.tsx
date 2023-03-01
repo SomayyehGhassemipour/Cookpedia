ca06f277e9ddbd48097860d17281162ab6bc3e23
Somayyeh Ghassemipour <somayyeh.ghassemipour@gmail.com>
2023-03-01 15:58:31 +0100

    added some details for theme

--- src/components/utilities/CheckBox.tsx
+++ src/components/utilities/CheckBox.tsx
@@ -1,10 +1,10 @@
 interface Props {
-  label: string
+  label: string  
 }
 export const CheckBox : React.FC<Props> = ({label}) => {
   return (
     <label className="checkbox-container">{label}
-      <input type="checkbox" />
+      <input type="checkbox" checked/>
       <span className="checkmark"></span>
     </label>
   )
